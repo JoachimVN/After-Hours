473199c56698cd3ddbad420bdce553e48bbbcba6
Joachim Valdersnes Nilsen <joachivn@stud.ntnu.no>
2026-03-30 00:01:11 +0200

    feat: Splash screen with test music
    Added a black splash screen with music to make launch time faster and more engaging

--- src/main/java/edu/ntnu/idatt2003/g23/App.java
+++ src/main/java/edu/ntnu/idatt2003/g23/App.java
@@ -19,11 +19,18 @@ import edu.ntnu.idatt2003.g23.model.Stock;
 //     }
 // }
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import edu.ntnu.idatt2003.g23.ui.HomePageView;
 /**
@@ -40,7 +47,16 @@ public class App extends Application {
      */
     @Override
     public void start(Stage stage) {
-        scene = new Scene(HomePageView.build(() -> {}), 1024, 768);
+        StackPane root = new StackPane();
+        root.getChildren().add(HomePageView.build(() -> {}));
+
+        Rectangle splash = new Rectangle();
+        splash.setFill(Color.BLACK);
+        splash.widthProperty().bind(root.widthProperty());
+        splash.heightProperty().bind(root.heightProperty());
+        root.getChildren().add(splash);
+
+        scene = new Scene(root, 1024, 768);
         scene.getStylesheets().add(getClass().getResource("/home.css").toExternalForm());
 
         Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
@@ -55,6 +71,25 @@ public class App extends Application {
         stage.setMinHeight(620);
         stage.setMaximized(true);
         stage.show();
+
+        FadeTransition fade = new FadeTransition(Duration.seconds(1.5), splash);
+        fade.setFromValue(1.0);
+        fade.setToValue(0.0);
+        fade.setOnFinished(e -> root.getChildren().remove(splash));
+
+        try {
+            String musicPath = getClass().getResource("/sound/music/idatt2003_sound_test2.mp3").toExternalForm();
+            MediaPlayer mediaPlayer = new MediaPlayer(new Media(musicPath));
+            mediaPlayer.setOnPlaying(() -> {
+                fade.setDelay(Duration.seconds(2.0));
+                fade.play();
+            });
+            mediaPlayer.play();
+        } catch (Exception e) {
+            // fallback: just fade if music fails to load
+            fade.setDelay(Duration.seconds(0.3));
+            fade.play();
+        }
     }
 
 
