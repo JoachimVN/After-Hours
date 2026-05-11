76e5791b868ea945d2a0e78e22297434322e4744
Joachim Valdersnes Nilsen <joachivn@stud.ntnu.no>
2026-05-11 12:11:51 +0200

    fix: Old tests

--- src/main/java/edu/ntnu/idatt2003/g23/ui/views/game/GameController.java
+++ src/main/java/edu/ntnu/idatt2003/g23/ui/views/game/GameController.java
@@ -379,12 +379,10 @@ public final class GameController {
 
   public void setPlayerStatusOverride(PlayerStatus status) {
     statusOverride = status;
-    player.setStatus(status);
   }
 
   public void clearPlayerStatusOverride() {
     statusOverride = null;
-    player.calculateStatus();
   }
 
   public boolean isOwned(String symbol) {
